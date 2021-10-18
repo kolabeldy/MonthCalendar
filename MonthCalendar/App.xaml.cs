9ffddf2e462b7873cfda92790a1b7818d189e88d
Kazimir <avvlasov@outlook.com>
2021-10-18 11:28:42 +0300

    Добавьте файлы проекта.

---
+++ MonthCalendar/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace MonthCalendar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                        new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
+
+    }
+}
